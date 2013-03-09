515c94f3fd57d2bb840bfc0cf6a11bdf9a10a09d
dee.ambler <duygu.ambler@gmail.com>
2013-03-09 13:36:30 -0800

    vote

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,12 @@
+class VotesController < ApplicationController
+end
+
+
+class VotesController < ApplicationController
+  def create
+    topic = Topic.find(params[:topic_id])
+    vote = topic.votes.build
+    vote.save!
+    redirect_to(topics_path)
+  end
+end
